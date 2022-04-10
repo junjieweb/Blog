c064f722a45faccfbc595e365cd9c3a5b1125298
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-04-11 02:35:13 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:mqyqingfeng/learn-typescript.git master:gh-pages
 
-git push -f git@github.com:junjieweb/Front-end-Developer.git main:gh-pages
+git push https://github.com/junjieweb/Front-end-Developer.git main:gh-pages
 
 cd -
